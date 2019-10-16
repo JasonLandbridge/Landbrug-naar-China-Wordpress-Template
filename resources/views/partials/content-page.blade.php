6c6e166ef7060f653369fcc4f967652040871cb4
JasonLandbridge <jlandbrug@protonmail.com>
2019-10-16 17:16:32 +0200

    Upgrades Sage from 9.0.5 to 9.0.9

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,1 +1,2 @@
-@php echo apply_filters('the_content', $post->post_content);@endphp
+@php the_content() @endphp
+{!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
